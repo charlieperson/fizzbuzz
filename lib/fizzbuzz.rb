3eac5d82187e1905f3e325f8906297a006836d55
Charlie Person <charlie.person@hyperisland.se>
2015-11-19 11:22:17 +0000

    returns buzz if divisible by 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,4 @@
 def fizzbuzz(num)
   return "fizz" if num % 3 == 0
+  return "buzz" if num % 5 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require "./lib/fizzbuzz.rb"
 
 describe "fizzbuzz" do
+
   it "if divisible by 3, return fizz" do
     expect(fizzbuzz(3)).to eq("fizz")
   end
+
+  it "if divisible by 5, return buzz" do
+    expect(fizzbuzz(5)).to eq("buzz")
+  end
+  
 end
