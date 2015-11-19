dfc2ec10ba24b80b65a54afcec101967acda6c11
Charlie Person <charlie.person@hyperisland.se>
2015-11-19 11:29:10 +0000

    fizzbuzz returns number if not divisible by 3 or 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,5 +9,12 @@ describe "fizzbuzz" do
   it "if divisible by 5, return buzz" do
     expect(fizzbuzz(5)).to eq("buzz")
   end
-  
+
+  it "if divisible by 15, return fizzbuzz" do
+    expect(fizzbuzz(15)).to eq("fizzbuzz")
+  end
+
+  it "should return the number if not divisible by 3 or 5" do
+    expect(fizzbuzz(2)).to eq(2)
+  end
 end
